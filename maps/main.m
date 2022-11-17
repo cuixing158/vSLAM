489a7303c15c9a71407d8a6d80fcbe61f6e0c920
cuixing158 <1229261804@qq.com>
2022-11-17 20:05:09 +0800

    fix: 修复conflict

--- maps/main.m
+++ maps/main.m
@@ -1,6 +1,7 @@
 %% 使用openstreetmap 的图导入到matlab 中的drivingScenario进行cuboid simuliaion 和
 % UE simulination
-scenario = drivingScenario("SampleTime",0.01,"StopTime",3);
+% driving scenario app中导入少量路径的openstreetmap,操作各类控件太慢。
+scenario = drivingScenario("SampleTime",0.01,"StopTime",inf);
 roadNetwork(scenario,"OpenStreetMap","my_openstreetmap_export.xml");
 egovhicle = vehicle(scenario,ClassID=1);
 egopath = [-133,-388,0;-98,-266,0];
